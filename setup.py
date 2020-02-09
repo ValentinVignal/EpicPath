50197c271b05915eda479f38aba3ee801399c99e
Valentin <valentin.vignal@gmail.com>
2020-02-10 00:27:51 +0800

    Update .gitignore and setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='epic-path',
+    version='0.0.0',
+    url='https://github.com/ValentinVignal/EpicPath.git',
+    license='MIT',
+    author='Valentin Vignal',
+    author_email='valentin.vignal.dev@outlook.fr',
+    description='A simple high level library to work with Path',
+    packages=find_packages(exclude=['tests', 'trash']),
+    long_description=open('README.md').read(),
+    zip_safe=False
+)
