cdb2612ca6ed3da9ba4595a9d562ea939d1f9a2c
Valentin <valentin.vignal@gmail.com>
2020-02-24 23:30:42 +0800

    Version 0.0.5

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='epic-path',
-    version='0.0.4',
+    version='0.0.5',
     url='https://github.com/ValentinVignal/EpicPath.git',
     license='MIT',
     author='Valentin Vignal',
     author_email='valentin.vignal.dev@outlook.fr',
     description='A simple high level library to work with Path',
     packages=find_packages(exclude=['tests', 'trash']),
+    long_description_content_type='text/markdown',
     long_description=open('README.md').read(),
     zip_safe=False
 )
