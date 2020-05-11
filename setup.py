f35e5f2c78822d8264c572c622ab30f8439874b7
ValentinVignal <valentin.vignal.dev@outlook.fr>
2020-05-11 09:06:05 +0800

    Version 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='epic-path',
-    version='0.0.7',
+    version='0.1.0',
     url='https://github.com/ValentinVignal/EpicPath.git',
     license='MIT',
     author='Valentin Vignal',
